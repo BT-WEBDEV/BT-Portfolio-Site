9e1f9c4fd8e6d0e2532aa64921fff35b5363ce28
Brandon Tetrick <brandontetrick@gmail.com>
2020-08-27 15:22:07 -0600

    Get by Latest View Change

--- projects/views.py
+++ projects/views.py
@@ -5,7 +5,7 @@ from projects.models import Project, Category
 
 #Project Index - Shows A Snippet of Information About Each Project
 def project_index(request): 
-    projects = Project.objects.latest()
+    projects = Project.objects.latest('get_by_latest')
     context = {
         'projects' : projects
     }
