8121529be0727b91ed0062874955d89956bafbdf
Brandon Tetrick <brandontetrick@gmail.com>
2020-08-27 13:35:09 -0600

    Models & Design

--- projects/models.py
+++ projects/models.py
@@ -5,10 +5,17 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+         
 class Project(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     categories = models.ManyToManyField('Category', related_name='projects')
     github = models.URLField(max_length=200, default="Enter Link Here", blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
